d79d2eebb6d85c0f4d5fb19c8134d1c98aa810f3
munjala <aarti.munjal@ucdenver.edu>
2015-08-01 22:22:24 -0600

    added audio files to play for the last screen.

--- SurgeryProtocols/Src/ViewControllers/OrEndController.m
+++ SurgeryProtocols/Src/ViewControllers/OrEndController.m
@@ -8,16 +8,38 @@
 
 #import <Foundation/Foundation.h>
 #import "orEndController.h"
+#import <AVFoundation/AVFoundation.h>
+
+@interface OrEndController()
+{
+    AVAudioPlayer *_audioPlayer;
+}
+@end
 
 @implementation OrEndController
 
 -(void)viewDidLoad
 {
+    NSString *path = [NSString stringWithFormat:@"%@/Voicemail.wav", [[NSBundle mainBundle] resourcePath]];
+    NSURL *soundUrl = [NSURL fileURLWithPath:path];
+    
+    // Create audio player object and initialize with URL to sound
+    _audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:soundUrl error:nil];
+    [_audioPlayer play];
+    
     self.orEndButton.cornerRadius = 10.0;
     self.orEndButton.shadowHeight = self.orEndButton.frame.size.height * 0.17;
     self.orEndButton.buttonColor = [UIColor ht_bitterSweetColor];
     self.orEndButton.shadowColor = [UIColor ht_bitterSweetDarkColor];
     [self.orEndButton setTitle:@"OR" forState:UIControlStateNormal];
+    self.orEndButton.userInteractionEnabled = NO;
 }
+
+-(void)viewDidAppear
+{
+    NSLog(@"in appear\n");
+    [_audioPlayer play];
+}
+
 @end
 
--- SurgeryProtocols/Src/ViewControllers/deadEnd.m
+++ SurgeryProtocols/Src/ViewControllers/deadEnd.m
@@ -8,11 +8,28 @@
 
 #import <Foundation/Foundation.h>
 #import "deadEnd.h"
+#import <AVFoundation/AVFoundation.h>
+
+
+@interface deadEnd()
+{
+    AVAudioPlayer *_audioPlayer;
+}
+
+@end
 
 @implementation deadEnd
 
 -(void)viewDidLoad
 {
+    
+    NSString *path = [NSString stringWithFormat:@"%@/beep.wav", [[NSBundle mainBundle] resourcePath]];
+    NSURL *soundUrl = [NSURL fileURLWithPath:path];
+    
+    // Create audio player object and initialize with URL to sound
+    _audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:soundUrl error:nil];
+    [_audioPlayer play];
+    
     
     self.deadEndButton.cornerRadius = 10.0;
     self.deadEndButton.shadowHeight = self.deadEndButton.frame.size.height * 0.17;
@@ -20,6 +37,7 @@
     self.deadEndButton.buttonColor = [UIColor ht_bitterSweetColor];
     self.deadEndButton.shadowColor = [UIColor ht_bitterSweetDarkColor];
     [self.deadEndButton setTitle:@"DEAD" forState:UIControlStateNormal];
+    self.deadEndButton.userInteractionEnabled = NO;
 }
 
 @end
