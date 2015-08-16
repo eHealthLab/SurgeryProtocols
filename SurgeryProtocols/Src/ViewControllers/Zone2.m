bc20d7dccadf8c400e91eb5b45ecf89f9010949a
aarti <aarti.munjal@gmail.com>
2015-08-16 14:05:14 -0600

    last commit before the machine dies

--- SurgeryProtocols/Src/ViewControllers/Zone2.m
+++ SurgeryProtocols/Src/ViewControllers/Zone2.m
@@ -7,3 +7,67 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Zone2.h"
+
+@implementation Zone2
+
+
+-(void)viewDidLoad
+{
+    self.zone2Array = [[NSArray alloc] initWithObjects:@"Yes", @"No", nil];
+    
+    self.zone2View.delegate = self;
+    self.zone2View.dataSource = self;
+    
+    [self.zone2View selectRow:0 inComponent:0 animated:NO];
+    
+    [self.nextButton setStyle:HTPressableButtonStyleRounded];
+    self.nextButton.buttonColor = [UIColor ht_bitterSweetColor];
+    self.nextButton.shadowColor = [UIColor ht_bitterSweetDarkColor];
+    [self.nextButton setStyle:HTPressableButtonStyleRounded];
+    [self.nextButton setShadowHeight:0.8];
+    [self.nextButton setTitle:@"Next" forState:UIControlStateNormal];
+    
+}
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 1;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    return [self.zone2Array count];
+}
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    return [self.zone2Array objectAtIndex:row];
+}
+
+- (IBAction)nextButtonPressed:(id)sender {
+    
+    NSString *choice = [self.zone2Array objectAtIndex:[self.zone2View selectedRowInComponent:0]];
+    if ([choice isEqualToString:@"Yes"]) {
+        UIViewController *uiViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"sbpController"];
+        
+        UIBarButtonItem *barButton = [[UIBarButtonItem alloc] init];
+        barButton.title=@"Back";
+        
+        self.navigationController.navigationBar.topItem.backBarButtonItem = barButton;
+        
+        [self.navigationController pushViewController:uiViewController animated:YES];
+    }
+    else {
+        UIViewController *uiViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"deadEnd"];
+        
+        UIBarButtonItem *barButton = [[UIBarButtonItem alloc] init];
+        barButton.title=@"Back";
+        
+        self.navigationController.navigationBar.topItem.backBarButtonItem = barButton;
+        
+        [self.navigationController pushViewController:uiViewController animated:YES];
+    }
+}
+@end
+
